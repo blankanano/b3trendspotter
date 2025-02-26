7c9f74fae3b25e92c8c90caddebd1e9daaa399a6
Adriano da Silva <adrianodas@gmail.com>
2025-02-26 18:10:47 +0000

    Atualizações

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 
 
 # python -m venv venv
+# streamlit run app.py
 
 # pip install streamlit
 # pip install yfinance plotly streamlit-extras
--- app.py
+++ app.py
@@ -3,17 +3,18 @@ import pandas as pd
 import numpy as np
 import yfinance as yf
 import plotly.express as px
+import matplotlib.pyplot as plt
 from datetime import datetime, time
 from streamlit_extras.metric_cards import style_metric_cards
 from streamlit_extras.grid import grid
 
-data_hoje = datetime.today();
+
+data_hoje = datetime.today()
+periodo_RSI = 14
 
 def build_sidebar():
     st.image("imagens/Logo_B3.png")
     ticker_list = pd.read_csv("ativos/tickers.csv", index_col=0)
-    tickers = st.multiselect(label="Selecione o par de moedas", options=ticker_list, placeholder='Ativos')
-    interval = st.selectbox('Selecione o intervalo de tempo', ('1m', '5m', '15m', '30m', '1h', '1d'), key='interval')
 
     col1, col2 = st.columns(2)
     with col1:
@@ -30,20 +31,98 @@ def build_sidebar():
     start_datetime = datetime.combine(start_date, start_time)
     end_datetime = datetime.combine(end_date, end_time)
 
-    if tickers:
-        dados_ativo = yf.download(tickers=tickers, start=start_datetime, end=end_datetime, interval=interval)["Close"]
-        return tickers, dados_ativo
-    return None, None
+    interval = st.selectbox('Selecione o intervalo de tempo', ('1m', '5m', '15m', '30m', '1h', '1d'), key='interval')
 
-def build_main(tickers, dados_ativo):
+    tickers = st.multiselect(label="Selecione o par de moedas", options=ticker_list, placeholder='Ativos')
+
+    if tickers:
+        
+        data_compra = []
+        data_venda = []
+
+        #try:
+        dados_ativo = yf.download(tickers=tickers, start=start_datetime, end=end_datetime, interval=interval)[['Close']]
+
+        if dados_ativo.empty:
+            st.error("Não foram encontrados dados para os parâmetros fornecidos.")
+            return None, None, None, None         
+
+        # Calcular as variações dos preços
+        dados_ativo['Retornos'] = dados_ativo['Close'].diff()
+
+        # Separar as variações em ganhos e perdas
+        dados_ativo['Ganhos'] = np.where(dados_ativo['Retornos'] > 0, dados_ativo['Retornos'], 0)
+        dados_ativo['Perdas'] = np.where(dados_ativo['Retornos'] < 0, abs(dados_ativo['Retornos']), 0)
+
+        # Calcular a media móvel dos ganhos e perdas
+        dados_ativo['Media_Ganhos'] = dados_ativo['Ganhos'].rolling(window=periodo_RSI).mean()
+        dados_ativo['Media_Perdas'] = dados_ativo['Perdas'].rolling(window=periodo_RSI).mean()
+
+        # Calcular o RS (Relative Strength)
+        dados_ativo['RS'] = dados_ativo['Media_Ganhos'] / dados_ativo['Media_Perdas']
+
+        # Calcular o RSI
+        dados_ativo['RSI'] = 100 - (100 / (1 + dados_ativo['RS']))
+
+        # Verificar sinais de compra
+        dados_ativo.loc[dados_ativo['RSI'] < 30, 'Compra'] = 'S' 
+        dados_ativo.loc[dados_ativo['RSI'] > 30, 'Compra'] = 'N'
+
+        for i in range(len(dados_ativo) - 1):
+            if pd.notna(dados_ativo['Compra'].iloc[i]) and "S" in dados_ativo['Compra'].iloc[i]:
+                if i + 1 < len(dados_ativo):
+                    data_compra.append(dados_ativo.iloc[i+1].name) # +1 porque compramos no preço de abertura do dia seguinte.
+            
+                for j in range(1, 11):
+                    if i + j + 1 < len(dados_ativo):
+                        if dados_ativo['RSI'].iloc[i + j] > 40: #vendo se nos próximos 10 dias o RSI passa de 40
+                            data_venda.append(dados_ativo.iloc[i + j + 1].name) #vende no dia seguinte que bater 40
+                            break
+                        elif j == 10:
+                            data_venda.append(dados_ativo.iloc[i + j + 1].name)
+
+        #except Exception as e:
+            #st.error(f"Ocorreu um erro ao processar os dados: {e}")        
+
+        return tickers, dados_ativo, data_compra, data_venda
+    return None, None, None, None
+
+def build_main(tickers, dados_ativo, data_compra, data_venda):
     st.dataframe(dados_ativo)
+    st.dataframe(data_compra)
+    st.dataframe(data_venda)
+
+    plt.figure(figsize = (12, 5))
+    plt.scatter(dados_ativo.loc[data_compra].index, dados_ativo.loc[data_compra]['Close'], marker = '^',
+    c = 'g')
+    plt.plot(dados_ativo['Close'], alpha = 0.7)   
+
+    st.pyplot(plt)
+
+    # Qual a média de lucros?
+    # Qual a média de perdas?
+    # Qual a % de operações vencedoras?
+    # Qual expectativa matemática do modelo?
+    # Qual retorno acumulado?
+    # O retorno acumulado venceu o Buy and Hold na ação?
+
+    # lucros = dados_ativo.loc[data_venda]['Open'].values/dados_ativo.loc[data_compra]['Open'].values - 1
+
+    # operacoes_vencedoras = len(lucros[lucros > 0])/len(lucros)
+    # media_ganhos = np.mean(lucros[lucros > 0])
+    # media_perdas = abs(np.mean(lucros[lucros < 0]))
+    # expectativa_matematica_modelo = (operacoes_vencedoras * media_ganhos) - ((1 - operacoes_vencedoras) * media_perdas)
+    # performance_acumulada = (np.cumprod((1 + lucros)) - 1)
+
+    # plt.figure(figsize = (12, 5))
+    # plt.plot(data_compra, performance_acumulada)    
 
 st.set_page_config(layout="wide")
 
 with st.sidebar:
-    tickers, dados_ativo = build_sidebar()
+    tickers, dados_ativo, data_compra, data_venda = build_sidebar()
 
 st.title('Identificador de tendências')
 
 if tickers:
-    build_main(tickers, dados_ativo)    
+    build_main(tickers, dados_ativo, data_compra, data_venda)    
